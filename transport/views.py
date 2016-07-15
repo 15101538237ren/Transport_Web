3bba34dfa049b401a16abeb7dd5a49674c73dc2d
jhljx <1261542265@qq.com>
2016-07-15 20:08:54 +0800

    修改了选取出的path的json格式

--- transport/views.py
+++ transport/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 from Transport_Web.helpers import *
 from Transport_Web.settings import STATIC_ROOT
 # Create your views here.
+
 def index(request):
     return render(request, 'transport/baidu.html',locals())
 def region(request):
@@ -12,7 +13,8 @@ def region(request):
     slng=float(request.GET.get("slng"))
     elat=float(request.GET.get("elat"))
     elng=float(request.GET.get("elng"))
-    print u"左下经纬度："+ unicode(slat)+u"," + unicode(slng) + u", 右上经纬度:"+unicode(elat)+u","+unicode(elng)
+    #print "左下经纬度："+ unicode(slat)+u"," + unicode(slng) + u", 右上经纬度:"+unicode(elat)+u","+unicode(elng)
+    #print("左下经纬度：" + str(slat) + u"," + str(slng) + u", 右上经纬度:" + str(elat) + u"," + str(elng))
 
     table_arr=load_pickle_from(STATIC_ROOT+os.sep+"WFJBXX_ORG.pkl")
     table_1=table_arr[0]
