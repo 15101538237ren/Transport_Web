dc71b8d0973a00ffaaa8aaf5dd97d8c92741520d
jhljx <1261542265@qq.com>
2016-07-19 16:15:28 +0800

    修改了生成的标注点文件的名称

--- Transport_Web/helpers.py
+++ Transport_Web/helpers.py
@@ -236,7 +236,7 @@ def poly_line_js(roads_set,roads_directions):
 if __name__ == '__main__':
     excel_path=STATIC_ROOT+os.sep+"WFJBXX_ORG.xls"
     out_pickle_path=STATIC_ROOT+os.sep+"WFJBXX_ORG.pkl"
-    #data_read_and_store(excel_path,out_pickle_path)
+    data_read_and_store(excel_path,out_pickle_path)
     #
     # excel_exception_path = STATIC_ROOT+os.sep+"exception.xlsx"
     # out_exception_pickle_path = STATIC_ROOT + os.sep + "Exception.pkl"
@@ -245,8 +245,8 @@ if __name__ == '__main__':
     # #将road_path整个目录下的path都存储成pkl格式
     out_road_path=STATIC_ROOT+os.sep+'path.pkl'
     road_read_and_store(ROAD_DIR,out_road_path)
-    # out_newdata_path = STATIC_ROOT + os.sep + 'newdata.pkl'
-    # label_points(out_pickle_path, out_road_path, out_newdata_path)
+    out_labeled_points_path = STATIC_ROOT + os.sep + 'labeledpoints.pkl'
+    label_points(out_pickle_path, out_road_path, out_labeled_points_path)
     #
     #
     # out_exception_data_path = STATIC_ROOT + os.sep + 'exceptiondata.pkl'
