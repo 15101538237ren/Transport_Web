fb5897f281e3b42654b45cf8e94734c1196e8c0a
jhljx <1261542265@qq.com>
2016-07-23 22:54:42 +0800

    123

--- transport/tests.py
+++ transport/tests.py
@@ -53,12 +53,18 @@ def upper_bound_search1(table,l,r,num):
     return l
 
 
-table = [1,2,3,3,3,3,3,4,5,6,6,6,6,7,8]
+'''table = [1,2,3,3,3,3,3,4,5,6,6,6,6,7,8]
 
 num = 6
 
 print(lower_bound_search(table,0,len(table),num))
 print(lower_bound_search1(table,0,len(table),num))
 print(upper_bound_search(table,0,len(table),num))
-print(upper_bound_search1(table,0,len(table),num))
+print(upper_bound_search1(table,0,len(table),num))'''
+
+
+
+
+
+
 
