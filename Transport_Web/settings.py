fc22d00a982e1fc4f3b4b201513fa371ea319957
jhljx <1261542265@qq.com>
2016-07-30 16:55:29 +0800

    change the json format

--- Transport_Web/settings.py
+++ Transport_Web/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.core.context_processors.static',
             ],
+            'debug':DEBUG,
         },
     },
 ]
